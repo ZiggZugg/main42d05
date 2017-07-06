fea47267b00c1d2705ef919087d24e9645a8a2ff
Alan Luu <wakkachao@gmail.com>
2017-07-06 12:59:42 -0700

    Added zzheaders.h

---
+++ zzheaders.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+//Just #include zzheaders.h to have all headers prepped. Format for prototypes as below
+//extern void myprototype(char *str);
